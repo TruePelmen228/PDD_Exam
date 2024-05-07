f287e29c3551c26056df740a26702544b109e265
Ёж Соник <g.mens.hl@gmail.com>
2024-05-07 21:45:12 +0300

    New bugs update

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 import sys
 import headder
 from PyQt6.QtGui import QPixmap, QFont
-from PyQt6.QtWidgets import  QDialog, QTableWidgetItem, QTableWidget, QApplication, QMainWindow, QPushButton, QLabel, QCheckBox
+from PyQt6.QtWidgets import  QDialog, QTableWidgetItem, QTableWidget, QApplication, QMainWindow, QPushButton, QLabel, QCheckBox, QMessageBox, QButtonGroup
 from PyQt6.QtCore import QTimer
 
 
@@ -53,79 +53,85 @@ class MainWindow(QMainWindow):
         button_st1.move(580,400)
         self.start_wd.append(button_st1)
         
-        
+
     def exam(self):
-        
         self.sender().hide()
         self.start_wd[0].hide()
         for i in range(len(self.queue_q)):
-                masas=[]           
-                label = QLabel(self.queue_q[self.g+i][2], self)
-                lb = QLabel(self)
-                label.setWordWrap(True)
-                label.resize(785, 100)
-                #print(str(self.queue_q[self.g+i][3]))
-                if str(self.queue_q[self.g+i][3])!='-':
-                    print(str(self.queue_q[self.g+i][3]))
-                    lb.setPixmap(QPixmap(str(self.queue_q[self.g+i][3])))
-                    lb.move(65, 110)
-                    lb.resize(645, 250)
-                #self.setCentralWidget(lb)
-                #self.resize(image.width(), image.height())
-                label.hide()
-                lb.hide()
-                masas.append(label)
-                masas.append(lb)
-                a1 =QCheckBox(self)
-                a2 =QCheckBox(self)
-                a3 =QCheckBox(self)
-                ot=50
-                a1.stateChanged.connect(self.onStateChanged)
-                a2.stateChanged.connect(self.onStateChanged)
-                a3.stateChanged.connect(self.onStateChanged)
-                sas=[a1, a2, a3]
-                hg=60
-                if self.queue_q[self.g+i][7]!="-":
-                    a4 =QCheckBox(self)
-                    a4.stateChanged.connect(self.onStateChanged)
-                    sas.append(a4)
-                    ot=10
-                    hg=55
-                for j in range(len(sas)):
-                        sas[j].setText(self.queue_q[self.g+i][j+4])
-                        sas[j].resize(510, 80)
-                        sas[j].move(50, 550-ot-hg*j)
-                        sas[j].hide()
-                        word_wrap(sas[j])
-                        masas.append(sas[j])
-                
-                
-                self.big_queue.append(masas)
-                
-                
-        #print(len(self.big_queue))
-        
+            masas = []           
+            label = QLabel(self.queue_q[self.g+i][2], self)
+            lb = QLabel(self)
+            label.setWordWrap(True)
+            label.resize(785, 100)
+            if str(self.queue_q[self.g+i][3]) != '-':
+                print(str(self.queue_q[self.g+i][3]))
+                lb.setPixmap(QPixmap(str(self.queue_q[self.g+i][3])))
+                lb.move(65, 110)
+                lb.resize(645, 250)
+            label.hide()
+            lb.hide()
+            masas.append(label)
+            masas.append(lb)
+            a1 = QCheckBox(self)
+            a2 = QCheckBox(self)
+            a3 = QCheckBox(self)
+            ot = 50
+            a1.stateChanged.connect(self.onStateChanged)
+            a2.stateChanged.connect(self.onStateChanged)
+            a3.stateChanged.connect(self.onStateChanged)
+            sas = [a1, a2, a3]
+            hg = 60
+            if self.queue_q[self.g+i][7] != "-":
+                a4 = QCheckBox(self)
+                a4.stateChanged.connect(self.onStateChanged)
+                sas.append(a4)
+                ot = 10
+                hg = 55
+            group = QButtonGroup()
+            a = 0
+            for checkbox in sas:
+                checkbox.setText(self.queue_q[self.g+i][sas.index(checkbox) + 4])
+                checkbox.resize(510, 80)
+                checkbox.move(50, 550-ot-hg*a)
+                checkbox.hide()
+                word_wrap(checkbox)
+                masas.append(checkbox)
+                group.addButton(checkbox)
+                a+=1
+            self.big_queue.append(masas)
+            group.setExclusive(True)
+
         for i in range(len(self.big_queue[self.g])):
             self.big_queue[self.g][i].show()
-        button_sl = QPushButton("->", self)
-        button_sl.setCheckable(True)
-        button_sl.resize(60, 60)
-        button_sl.clicked.connect(self.the_button_was_clicked)
-        button_sl.move(620,500)
-        button_sl.show()
-        button_s2 = QPushButton("<-", self)
-        button_s2.setCheckable(True)
-        button_s2.resize(60, 60)
-        button_s2.clicked.connect(self.the_button_was_clicked)
-        button_s2.move(560,500)
-        button_s2.show()
- 
+        
+            button_sl = QPushButton("➡️", self)
+            button_sl.setCheckable(True)
+            button_sl.resize(60, 60)
+            button_sl.clicked.connect(self.the_button_was_clicked)
+            button_sl.move(620,500)
+            button_sl.setStyleSheet("font-size: 15pt;")
+            button_sl.show()
+
+            button_s2 = QPushButton("⬅️", self)
+            button_s2.setCheckable(True)
+            button_s2.resize(60, 60)
+            button_s2.clicked.connect(self.the_button_was_clicked)
+            button_s2.move(560,500)
+            button_s2.setStyleSheet("font-size: 15pt;")
+            button_s2.show()
+
     def the_button_was_clicked(self):
             if len(self.ans_q)<=self.g:
-                dlg = QDialog(self)
-                dlg.setWindowTitle("ВЫБИРИТЕ ВАРИАНТ ОТВЕТА!")
-                dlg.resize(400, 50)
-                dlg.exec()
+                
+                #dlg = QDialog(self)
+                #dlg.setWindowTitle("ВЫБИРИТЕ ВАРИАНТ ОТВЕТА!")
+                #dlg.resize(400, 50)
+                #dlg.exec()
+                
+                #Здесь возможно больше подойдёт это окно:
+                QMessageBox.information(self, "Экзамен по ПДД", "Пожалуйста, выберите вариант ответа!")
+
+                
             else:
                 for i in range(len(self.big_queue[self.g])):
                     self.big_queue[self.g][i].hide()
@@ -144,7 +150,9 @@ class MainWindow(QMainWindow):
                 rec.append(self.queue_q[self.g][8])                
                 self.tabel.append(rec)
                 self.g+=1
-                self.reload()
+                
+            self.reload()
+            self.sender().setChecked(False) 
     def reload(self):
         if self.g>=len(self.big_queue):
             for i in range(len(self.big_queue[self.g-1])):
@@ -159,6 +167,7 @@ class MainWindow(QMainWindow):
             if len(self.ans_q)>self.g:
                 self.ans_q.pop(self.g)       
             self.ans_q.append(self.sender().text())
+    
 
     def tabel_show(self):
         tb=QTableWidget(len(self.big_queue)+1, 4, self)
